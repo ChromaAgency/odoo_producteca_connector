1797f5f6a5086c293e0081c2c323541ae163a2d1
Matias Rivera <mrivera@chroma.agency>
2025-11-11 15:14:08 -0300

    fix: corregir la lógica de asignación de identificación y responsabilidad en la creación de empresas

--- producteca_connector/models/res_partner.py
+++ producteca_connector/models/res_partner.py
@@ -20,16 +20,18 @@ class ResPartner(models.Model):
             return partner
         company = self.env['res.partner'].sudo().search([('vat', '=', contact.billing_info.doc_number), ('parent_id', '=', False)], limit=1)
         if not company and contact.billing_info.doc_number:
-            # TODO: Migrate this to producteca_connector_l10n_ar
-            identification = self.env['l10n_latam.identification.type'].sudo().search([('name', '=', contact.billing_info.doc_type)], limit=1)
-            responsibility = self.env['l10n_ar.afip.responsibility.type'].sudo().search([('name', 'ilike', contact.billing_info.tax_payer_type)], limit=1)
+            identification = None
+            responsibility = None
+            
+            if 'l10n_latam.identification.type' in self.env:
+                identification = self.env['l10n_latam.identification.type'].sudo().search([('name', '=', contact.billing_info.doc_type)], limit=1)
+            
+            if 'l10n_ar.afip.responsibility.type' in self.env:
+                responsibility = self.env['l10n_ar.afip.responsibility.type'].sudo().search([('name', 'ilike', contact.billing_info.tax_payer_type)], limit=1)
+            
             state = self.env['res.country.state'].sudo().search([('name', '=', contact.billing_info.state)], limit=1)
             company_info = {
                 'name': contact.billing_info.business_name if contact.billing_info.business_name else contact.name,
-                # TODO: Migrate this to producteca_connector_l10n_ar
-                'l10n_latam_identification_type_id': identification.id,
-                # TODO: Migrate this to producteca_connector_l10n_ar
-                'l10n_ar_afip_responsibility_type_id': responsibility.id,
                 'vat': contact.billing_info.doc_number,
                 'street': f'{contact.billing_info.street_name} {contact.billing_info.street_number}',
                 'zip': contact.billing_info.zip_code,
@@ -38,6 +40,12 @@ class ResPartner(models.Model):
                 'country_id': self.env.ref('base.ar').id,
                 'company_type': 'company',
             }
+            
+            if identification:
+                company_info['l10n_latam_identification_type_id'] = identification.id
+            if responsibility:
+                company_info['l10n_ar_afip_responsibility_type_id'] = responsibility.id
+            
             company = self.env['res.partner'].sudo().create(company_info)
         contact_info = {
             'name': contact.name,
